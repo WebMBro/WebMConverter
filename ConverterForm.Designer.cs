37b7b238d71d45cec7729f09fd7e996bf5877669
WebMBro <nah@nah.nah>
2014-04-06 05:12:04 +0200

    first upload

---
+++ ConverterForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WebMConverter
+{
+    partial class ConverterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBoxOutput = new System.Windows.Forms.RichTextBox();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 31F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.textBoxOutput, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonCancel, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 31F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(678, 435);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tableLayoutPanel1.SetColumnSpan(this.textBoxOutput, 2);
+            this.textBoxOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxOutput.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxOutput.Location = new System.Drawing.Point(3, 3);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ReadOnly = true;
+            this.textBoxOutput.Size = new System.Drawing.Size(672, 398);
+            this.textBoxOutput.TabIndex = 2;
+            this.textBoxOutput.Text = "";
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonCancel.Location = new System.Drawing.Point(34, 407);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(641, 25);
+            this.buttonCancel.TabIndex = 3;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox.BackgroundImage = global::WebMConverter.Properties.Resources.clock;
+            this.pictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(3, 407);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox.TabIndex = 4;
+            this.pictureBox.TabStop = false;
+            // 
+            // ConverterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 441);
+            this.ControlBox = false;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "ConverterForm";
+            this.Padding = new System.Windows.Forms.Padding(3);
+            this.Text = "Conversion Progress";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ConverterForm_FormClosing);
+            this.Load += new System.EventHandler(this.ConverterForm_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox textBoxOutput;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.PictureBox pictureBox;
+
+
+    }
+}
---
+++ ConverterForm.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+namespace WebMConverter
+{
+    public partial class ConverterForm : Form
+    {
+        private string _arguments;
+        private Process _process;
+        private Timer _timer;
+        private bool _ended;
+        private bool _panic;
+
+        public ConverterForm(string arg)
+        {
+            InitializeComponent();
+
+            _arguments = arg;
+
+        }
+
+        private void ConverterForm_Load(object sender, EventArgs e)
+        {
+            textBoxOutput.AppendText("Starting...");
+            textBoxOutput.AppendText("\nArguments: " + _arguments);
+
+            string ffmpeg = Path.Combine(Environment.CurrentDirectory, "ffmpeg/ffmpeg.exe");
+
+            _process = new Process();
+
+            ProcessStartInfo info = new ProcessStartInfo(ffmpeg);
+            info.RedirectStandardInput = true;
+            info.RedirectStandardOutput = true;
+            info.RedirectStandardError = true;
+            info.UseShellExecute = false; //Required to redirect IO streams
+            info.CreateNoWindow = true; //Hide console
+            info.Arguments = _arguments;
+
+            _process.StartInfo = info;
+            _process.EnableRaisingEvents = true; //!!!!
+
+            _process.ErrorDataReceived += (o, args) =>
+                                             {
+                                                 if (args.Data != null)
+                                                     textBoxOutput.Invoke((Action)(() => textBoxOutput.AppendText("\n" + args.Data)));
+                                             };
+
+            _process.OutputDataReceived += (o, args) =>
+                                              {
+                                                  if (args.Data != null)
+                                                      textBoxOutput.Invoke((Action)(() => textBoxOutput.AppendText("\n" + args.Data)));
+                                              };
+
+            _process.Exited += (o, args) => textBoxOutput.Invoke((Action)(() =>
+                                                                              {
+                                                                                  if (_panic) return; //This should stop that one excetion when closing the converter
+                                                                                  textBoxOutput.AppendText("\n--- FFMPEG HAS EXITED ---");
+                                                                                  buttonCancel.Enabled = false;
+
+                                                                                  _timer = new Timer();
+                                                                                  _timer.Interval = 500;
+                                                                                  _timer.Tick += Exited;
+                                                                                  _timer.Start();
+                                                                              }));
+
+            _process.Start();
+            _process.BeginErrorReadLine();
+            _process.BeginOutputReadLine();
+
+            textBoxOutput.AppendText("\nffmpeg.exe is now converting your video.");
+
+            _process.StandardInput.Write("y\n"); //should confirm overwrite?
+        }
+
+        private void Exited(object sender, EventArgs eventArgs)
+        {
+            _timer.Stop();
+
+            if (_process.ExitCode != 0)
+            {
+                textBoxOutput.AppendText(string.Format("\n\nffmpeg.exe exited with exit code {0}. That's usually bad.", _process.ExitCode));
+                pictureBox.BackgroundImage = Properties.Resources.cross;
+            }
+            else
+            {
+                textBoxOutput.AppendText("\n\nVideo converted succesfully!");
+                pictureBox.BackgroundImage = Properties.Resources.tick;
+            }
+
+            buttonCancel.Text = "Close";
+           buttonCancel.Enabled = true;
+           _ended = true;
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            if (!_ended)
+            {
+                if (!_process.HasExited)
+                    _process.Kill();
+            }
+            else
+                Close();
+        }
+
+        private void ConverterForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //TODO: an exception gets thrown when the user closes the converter form while it's converting!
+            _panic = true;
+            buttonCancel_Click(sender, e);
+        }
+    }
+}
