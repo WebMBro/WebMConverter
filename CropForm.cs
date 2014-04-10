7202fbe07a232eeba192b39b305b08afd8239637
WebMBro <rothissu@outlook.com>
2014-04-10 17:56:46 +0200

    Cropping form stub, added warning for thread crash, now showing different error if cancelled
    Will work on the cropping form within a few days.

---
+++ CropForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WebMConverter
+{
+    public partial class CropForm : Form
+    {
+        public CropForm()
+        {
+            InitializeComponent();
+
+            //Stub for video crop form
+            //Template:
+                //-filter:v "crop=out_w:out_h:x:y"
+
+            //TODO: use this form to determine a cropping area for the video
+            //TODO: run ffmpeg here to get a picture of the first frame of the video, this way you can indirectly determine the size
+            //TODO: use the click/drag events to draw a rectangle that will be used for the cropping
+                //OR: have 4 lines that can be slided using the mouse? Probably bad idea, only allows symmetrical cropping.
+                //OR: draw a rectangle with 4 white corners, then the user can drag the corners to change the rectangle
+            //TODO: when pressing Confirm, put the values in a new text box that'll be added to the video options
+        }
+    }
+}
